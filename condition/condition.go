2dc1c8f6098ddf83fd842e0ff1b8d84afc1c51dd
Iqbal Nur W <iqbalnur306@gmail.com>
2020-09-07 08:43:01 +0700

    condition

---
+++ condition/condition.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	main := "iqbal"
+
+	if main == "iqbal"{
+		fmt.Println("Ini adalah iqbal")
+	}else{
+		fmt.Println("Ini bukan iqbal")
+	}
+
+	
+}
+
+// Referensi belajar condition di golang
+// https://medium.com/rungo/anatomy-of-conditional-statements-and-loops-in-go-aa84352cc34d
---
+++ condition/if_else.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	if angka := 10; angka > 2{
+
+		fmt.Println("ini angka lebih besar")
+	}else if angka < 2 {
+
+		fmt.Println("ini angka lebih kecil")
+	} else {
+
+		fmt.Println("not found")
+	}
+}
